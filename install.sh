362cc4165511b86cf43a0bbf887ce3a67b4220ae
Lester Hedges <lester.hedges@gmail.com>
2013-11-16 14:46:52 -0800

    Ammended indenting for readability.

--- install.sh
+++ install.sh
@@ -3,22 +3,22 @@
 
 # Checks for root permission.
 if test "`id -u`" -ne 0
-	then
-	echo "Please run the installer as root."
-	exit
+    then
+    echo "Please run the installer as root."
+    exit
 fi
 
 # Assign dependencies.
 if [[ `uname` == "Darwin" ]]; then
-	deps=(md5 pdflatex pdfcrop gs)
+    deps=(md5 pdflatex pdfcrop gs)
 else
-	deps=(md5sum pdflatex pdfcrop gs)
+    deps=(md5sum pdflatex pdfcrop gs)
 fi
 
 # Check dependencies.
 echo -n " Checking dependencies..."
 for pkg in ${deps[@]}; do
-	command -v $pkg >/dev/null 2>&1 || { echo -e "\n\t\e[1;34m\"$pkg\"\e[00m not found. \e[0;31mAborting.\e[00m" >&2; exit 1; }
+    command -v $pkg >/dev/null 2>&1 || { echo -e "\n\t\e[1;34m\"$pkg\"\e[00m not found. \e[0;31mAborting.\e[00m" >&2; exit 1; }
 done
 echo -e " \e[00;32mPassed.\e[00m"
 
@@ -26,46 +26,46 @@ echo -n " Installing..."
 
 # Install script.
 if [ ! -d /usr/local/bin ]; then
-	mkdir -p /usr/local/bin
+    mkdir -p /usr/local/bin
 fi
 if [[ `uname` == "Darwin" ]]; then
-	install -g 0 -o 0 -m 0755 latuxit /usr/local/bin/latuxit
+    install -g 0 -o 0 -m 0755 latuxit /usr/local/bin/latuxit
 else
-	install -Dm755 latuxit /usr/local/bin/
+    install -Dm755 latuxit /usr/local/bin/
 fi
 
 # Install man page.
 if [ ! -d /usr/local/man ]; then
-	mkdir -p /usr/local/man
+    mkdir -p /usr/local/man
 fi
 if [ ! -d /usr/local/man/man1 ]; then
-	mkdir -p /usr/local/man/man1
+    mkdir -p /usr/local/man/man1
 fi
 if [[ `uname` == "Darwin" ]]; then
-	install -g 0 -o 0 -m 0644 latuxit.1 /usr/local/man/man1/latuxit.1
+    install -g 0 -o 0 -m 0644 latuxit.1 /usr/local/man/man1/latuxit.1
 else
-	install -Dm644 latuxit.1 /usr/local/man/man1/
+    install -Dm644 latuxit.1 /usr/local/man/man1/
 fi
 gzip -f /usr/local/man/man1/latuxit.1
 
 # Set up latuxit directory tree if it doesn't exist.
 if [ ! -d ~/.latuxit ]; then
-	mkdir -p ~/.latuxit
+    mkdir -p ~/.latuxit
 fi
 
 if [ ! -d ~/.latuxit/cache ]; then
-	mkdir -p ~/.latuxit/cache
+    mkdir -p ~/.latuxit/cache
 fi
 
 if [ ! -f ~/.latuxit/latuxit.colors ]; then
-	cp latuxit.colors ~/.latuxit/latuxit.colors
+    cp latuxit.colors ~/.latuxit/latuxit.colors
 fi
 
 # Change latuxit directory permissions.
 if [[ `uname` == "Darwin" ]]; then
-	chown -R $(logname):staff ~/.latuxit
+    chown -R $(logname):staff ~/.latuxit
 else
-	chown -R $(logname):users ~/.latuxit
+    chown -R $(logname):users ~/.latuxit
 fi
 
 echo -e " \e[00;32mDone.\e[00m"
--- uninstall.sh
+++ uninstall.sh
@@ -3,26 +3,26 @@
 
 # Checks for root permission.
 if test "`id -u`" -ne 0
-	then
-	echo "Please run the uninstaller as root."
-	exit
+    then
+    echo "Please run the uninstaller as root."
+    exit
 fi
 
 echo -n " Uninstalling..."
 
 # Uninstall latuxit script.
 if [ -f /usr/local/bin/latuxit ]; then
-	rm /usr/local/bin/latuxit
+    rm /usr/local/bin/latuxit
 fi
 
 # Uninstall man page.
 if [ -f /usr/local/man/man1/latuxit.1.gz ]; then
-	rm /usr/local/man/man1/latuxit.1.gz
+    rm /usr/local/man/man1/latuxit.1.gz
 fi
 
 # Remove latuxit directory tree.
 if [ -d ~/.latuxit ]; then
-	rm -r ~/.latuxit
+    rm -r ~/.latuxit
 fi
 
 echo -e " \e[00;32mDone.\e[00m"
